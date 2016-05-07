5a8bb3aa88546ffbc61956349b583368b8a7fa61
JunkyKeks <JunkyKeks@freenet.de>
2016-05-07 16:50:11 +0200

    test
    test

--- src/main/java/de/fredo121/lobbyutils/main/LobbyUtils.java
+++ src/main/java/de/fredo121/lobbyutils/main/LobbyUtils.java
@@ -26,6 +26,55 @@ public class LobbyUtils extends JavaPlugin {
 		if (command.getName().equalsIgnoreCase("lobby") || command.getName().equalsIgnoreCase("hub")) {
 			Util.SendToBungeeServer(Settings.get().getBungeeLobbyServer(), (Player)sender);
 		}
+		
+		if (command.getName().equalsIgnoreCase("ts")) {
+		    if (args.length>1) {
+		        return false;
+		    }
+
+		    Player player = (Player) sender;
+		    player.sendMessage("§7[§6Toothwitch§7] §7Unsere TS3-IP lautet §6ts.toothwit.ch§7!");
+		    return true;    
+		 
+		}    
+
+		if (command.getName().equalsIgnoreCase("ts3")) {
+		    if (args.length>1) {
+		        return false;
+		    }
+
+		    Player player = (Player) sender;
+		    player.sendMessage("§7[§6Toothwitch§7] §7Unsere TS3-IP lautet §6ts.toothwit.ch§7!");
+		    return true;    
+		 
+		} 
+		   
+		if (command.getName().equalsIgnoreCase("vote")) {
+		    if (args.length>1) {
+		        return false;
+		    }
+
+		    Player player = (Player) sender;
+		    player.sendMessage("§7[§6Toothwitch§7] §7Vote für uns auf §6ts.toothwit.ch/vote§7!");
+		    return true;    
+		 
+		}
+		
+		if (command.getName().equalsIgnoreCase("web")) {
+		    if (args.length>1) {
+		        return false;
+		    }
+
+		    Player player = (Player) sender;
+		    player.sendMessage("§7[§6Toothwitch§7] §7Besuche unsere Website auf §6ts.toothwit.ch§7!");
+		    return true;    
+		 
+		}
+		
+
+
+		
+
 		return false; 
 	}
 
@@ -36,4 +85,5 @@ public class LobbyUtils extends JavaPlugin {
 	public LobbyUtils() {
 		instance = this;
 	}
+	
 }
